a6fa51a33c366bb95d359cb1eae595bb6934061c
PavanKalyanPasupuleti <146694292+PavanKalyanPasupuleti@users.noreply.github.com>
2026-04-08 12:05:54 +0530

    Create app.py

---
+++ server/app.py
@@ -0,0 +1,16 @@
+import sys
+import uvicorn
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent.parent))
+
+from openenv.core.env_server import create_fastapi_app
+from .environment import CloudOrchestratorEnvironment
+from models import OrchestratorAction, ClusterMetrics
+
+app = create_fastapi_app(CloudOrchestratorEnvironment, OrchestratorAction, ClusterMetrics)
+
+def main():
+    uvicorn.run("server.app:app", host="0.0.0.0", port=8000, factory=False)
+
+if __name__ == "__main__":
+    main()
